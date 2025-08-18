8bf3dd609a6982b9c65e03fabee065adf7bb0bdf
guijianwei0615 <guijianwei@huawei.com>
2025-08-18 08:36:19 +0000

    !159 feat: Byte类型支持 * fix：取消无意义的修改 * delete：删除float type * java文件缩进调整 * fix: java代码缩进调整 * delete logs * feat: Byte类型支持

--- cpp-omni/src/substrait/SubstraitToOmniExpr.cpp
+++ cpp-omni/src/substrait/SubstraitToOmniExpr.cpp
@@ -29,6 +29,8 @@ DataTypePtr GetScalarType(const ::substrait::Expression::Literal &literal)
     switch (typeCase) {
         case ::substrait::Expression_Literal::LiteralTypeCase::kBoolean:
             return BooleanType();
+        case ::substrait::Expression_Literal::LiteralTypeCase::kI8:
+            return ByteType();
         case ::substrait::Expression_Literal::LiteralTypeCase::kI16:
             return ShortType();
         case ::substrait::Expression_Literal::LiteralTypeCase::kI32:
@@ -252,6 +254,8 @@ TypedExprPtr SubstraitOmniExprConverter::ToOmniExpr(const ::substrait::Expressio
     switch (typeCase) {
         case ::substrait::Expression_Literal::LiteralTypeCase::kBoolean:
             return new LiteralExpr(substraitLit.boolean(), BooleanType());
+        case ::substrait::Expression_Literal::LiteralTypeCase::kI8:
+            return new LiteralExpr(static_cast<int8_t>(substraitLit.i8()), ByteType());
         case ::substrait::Expression_Literal::LiteralTypeCase::kI16:
             return new LiteralExpr(static_cast<int16_t>(substraitLit.i16()), ShortType());
         case ::substrait::Expression_Literal::LiteralTypeCase::kI32:
