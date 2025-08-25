8b24822b85ec6da11ca314996a1dd189a5578ecd
zhy61718 <11383867+zhy61718@user.noreply.gitee.com>
2025-08-25 09:34:05 +0000

    !164 add get_json_object Expr * add get_json_object Expr

--- cpp-omni/src/substrait/SubstraitParser.cpp
+++ cpp-omni/src/substrait/SubstraitParser.cpp
@@ -369,5 +369,6 @@ SubstraitParser::substraitOmniFunctionMap = {
     {"first_ignore_null", {FUNCTION_OMNI_EXPR_TYPE, "first_ignore_null"}},
     {"stddev_samp", {FUNCTION_OMNI_EXPR_TYPE, "stddev_samp"}},
     {"date_add", {FUNCTION_OMNI_EXPR_TYPE, "date_add"}},
-    {"trunc", {FUNCTION_OMNI_EXPR_TYPE, "trunc_date"}}};
+    {"trunc", {FUNCTION_OMNI_EXPR_TYPE, "trunc_date"}},
+    {"get_json_object", {FUNCTION_OMNI_EXPR_TYPE, "GetJsonObject"}}};
 } // namespace omniruntime
