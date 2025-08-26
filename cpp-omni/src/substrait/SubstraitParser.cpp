13567cadefea69a8e19a6f6fb8601802d7d29ea9
向欣伟 <xiangxinwei@h-partners.com>
2025-08-26 06:46:44 +0000

    !176 support regexp_extract * support regexp_extract

--- cpp-omni/src/substrait/SubstraitParser.cpp
+++ cpp-omni/src/substrait/SubstraitParser.cpp
@@ -365,6 +365,7 @@ SubstraitParser::substraitOmniFunctionMap = {
     {"power", {FUNCTION_OMNI_EXPR_TYPE, "power"}},
     {"first", {FUNCTION_OMNI_EXPR_TYPE, "first"}},
     {"substring_index", {FUNCTION_OMNI_EXPR_TYPE, "substring_index"}},
+    {"regexp_extract", {FUNCTION_OMNI_EXPR_TYPE, "regexp_extract"}},
     {"make_decimal", {FUNCTION_OMNI_EXPR_TYPE, "MakeDecimal"}},
     {"unix_timestamp", {FUNCTION_OMNI_EXPR_TYPE, "unix_timestamp"}},
     {"from_unixtime", {FUNCTION_OMNI_EXPR_TYPE, "from_unixtime"}},
