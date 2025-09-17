386615a5268a0400e78c83bfb7c8766f25efa8e3
dzs <12625183+wuxu_2023@user.noreply.gitee.com>
2025-09-17 06:39:18 +0000

    !189 [feat] support float type for shuffle * [feat] support float type for shuffle

--- backends-omni/src/main/scala/org/apache/gluten/expression/OmniExpressionAdaptor.scala
+++ backends-omni/src/main/scala/org/apache/gluten/expression/OmniExpressionAdaptor.scala
@@ -1218,6 +1218,8 @@ object OmniExpressionAdaptor extends Logging {
         TimestampDataType.TIMESTAMP
       case DoubleType =>
         DoubleDataType.DOUBLE
+      case FloatType =>
+        FloatDataType.FLOAT
       case BooleanType =>
         BooleanDataType.BOOLEAN
       case StringType =>
